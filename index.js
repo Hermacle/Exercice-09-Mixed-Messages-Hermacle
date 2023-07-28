6a7e90515d2ac74b739c2a853701300cb5e74910
Hermacle <kihenyeghohermacle@gmail.com>
2023-07-28 10:27:51 +0100

    add object that stores arrays

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function format(array) {
     const infoMessage = array.join(' ');
     return infoMessage;
 }
+
+//object that stores arrays 
+const mixedElements = {
+    temperature: ['30', '24', '36', '20', '40', '29', '31'],
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+};
+
