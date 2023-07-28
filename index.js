c20690d4c08abd8ada9259b0f93c5219951e1b08
Hermacle <kihenyeghohermacle@gmail.com>
2023-07-28 10:34:41 +0100

    add loop iterates

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ function format(array) {
 
 //object that stores arrays 
 const mixedElements = {
-    temperature: ['30', '24', '36', '20', '40', '29', '31'],
-    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    temperature: ['30', '24', '36', '20', '40', '29', '31']
 };
 
 //array that stores randomly choosed element
 let mixedMessage = [];
+
+
+for(let i in mixedElements) {
+    let index = randomNumber(mixedElements[i].length);
+
+    switch(i) {
+        case 'days':
+            mixedMessage.push(`Today is ${mixedElements[i][index]}`);
+            break;
+        case 'temperature':
+            mixedMessage.push(`and the temperature is ${mixedElements[i][index]} ° Celsius`);
+            break;
+        default:
+           console.log('Sorry! There is no Message today');
+    }
+}
+console.log(format(mixedMessage));
 
